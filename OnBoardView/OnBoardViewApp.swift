8bd96c7b7402a8a38fc115227a9c12c2621c9abf
n_leonov <65124365+n-leonov@users.noreply.github.com>
2022-03-26 00:34:02 +0300

    Initial Commit

---
+++ OnBoardView/OnBoardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardViewApp.swift
+//  OnBoardView
+//
+//  Created by Macbook on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
